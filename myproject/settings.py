786f1a2b01c15cbd5ed1087159cc6c5f62fb0b04
Kamal Rathi <kamalrathi049@gmail.com>
2022-01-14 19:07:17 +0530

    updated

--- myproject/settings.py
+++ myproject/settings.py
@@ -155,12 +155,12 @@ PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_HOST_USER = 'thejamesbond49@gmail.com'
-EMAIL_HOST_PASSWORD = 'jamesbond49@gmail'
+EMAIL_HOST_PASSWORD = 'fugkkjvspanlovgz'
 
 
 
